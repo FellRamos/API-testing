82d7a3bbe4733f7322f94db77ed7bf2bbe9fa6cf
FellRamos <36665175+FellRamos@users.noreply.github.com>
2018-11-08 09:37:09 +0100

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/5160fb7153c30a4c141c0177884e350fecd140b11fc6389ffcaae35dd69ffa3e/guestbook';
+const apiUrl = 'https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/5160fb7153c30a4c141c0177884e350fecd140b11fc6389ffcaae35dd69ffa3e/cloudant1';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
